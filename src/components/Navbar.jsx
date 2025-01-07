2061c8d449b7979a8445e45ad3a284e6b824377b
saurabhnathofficial <saurabhnathofficial01@gmail.com>
2025-01-07 21:34:47 +0530

    audio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 
 const navItems = ['Nexus', 'Vault', 'Prologues', 'About', 'Contact'];
 
 const Navbar = () => {
+    const [isAudioPlaying, setAudioPlaying] = useState(false);
+    const [isIndicatorActive, setIndicatorActive] = useState(false);
     const navContainerRef = useRef(null);
+    const audioElementRef = useRef(null);
+    
+    const toggleAudioIndicator = () => {
+        setAudioPlaying((prev) => !prev);
+        setIndicatorActive((prev) => !prev);
+    };
+
+    useEffect(() => {
+        if(isAudioPlaying){
+            audioElementRef.current.play();
+        }else{
+            audioElementRef.current.pause();
+        }
+    },[isAudioPlaying]);
+    
     return <div
         ref={navContainerRef}
         className="fixed inset-x-0 top-4 z-20 h-16 border-none trasition-all duration-700 sm:inset-x-6">
@@ -28,13 +45,17 @@ const Navbar = () => {
 
                 <div id="navLink" className="hidden md:block">
                     {navItems.map((item) => (
-                        <a className="nav-hover-btn">
+                        <a key={item} href={`#${item.toLowerCase()}`} className="nav-hover-btn">
                             {item}
                         </a>
                     ))}
-
                 </div>
-
+                <button className="ml-10 flex items-center space-x-0.5" onClick={toggleAudioIndicator}>
+                    <audio ref={audioElementRef} className="hidden" src="/audio/loop.mp3" loop />
+                        {[1,2,3,4].map((bar) => (
+                            <div key={bar} className={`indicator-line ${isIndicatorActive ? 'active' : ''}`} style={{animationDelay: `${bar * 0.1}s`}} />
+                        ))}
+                </button>
             </div>
         </nav>
 
